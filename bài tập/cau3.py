57e15255fa7319447cca35370bbeb9d1e90926ac
LeXuanHuy24 <lxhuy.24174600063@sv.uneti.edu.vn>
2024-10-29 18:10:31 +0700

    cau3.py

---
+++ bài tập/cau3.py
@@ -0,0 +1,6 @@
+#Viết chương trình tìm số lớn nhất trong 3 số bằng Python
+a = float(input("nhập số a: "))
+b = float(input("nhập số b: "))
+c = float(input("nhập số c: "))
+so_lon_nhat = max(a , b , c)
+print(f"số lớn nhất trong ba số: {so_lon_nhat}")
